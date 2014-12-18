547096675655f5bd7c3e932d65e7f167b0a26fc0
gogotanaka <mail@tanakakazuki.com>
2014-12-17 16:19:23 -0800

    Colorize

--- lib/enjoy_markov.rb
+++ lib/enjoy_markov.rb
@@ -1,11 +1,19 @@
 class EnjoyMarkov
-  def self.start!(supinf=20)
-    @@x = 0
-    loop do
-      @@x += [-1, 1].sample # TODO: use prefer randmGen
-      raise 'ops.. ;(' if supinf == @@x.abs
-      p %!#{"\s" * supinf}|#{"\s" * supinf}!.tap { |s| s[@@x + supinf] = '*' }
-      sleep 0.05
+  class << self
+    def start!(supinf=20)
+      @@x = 0
+      loop do
+        @@x += [-1, 1].sample # TODO: use prefer randmGen
+        raise 'ops.. ;(' if supinf == @@x.abs
+        puts "#{wall}#{space * supinf}#{axis}#{space * supinf}#{wall}".tap { |s| s[@@x + supinf + 1] = point }
+        sleep 0.05
+      end
     end
+
+    private
+      def axis;  "|"            end
+      def wall;  "|"            end
+      def point; "\e[31m*\e[0m" end
+      def space; "\s"           end
   end
 end
